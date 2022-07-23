603fb31f2f6f4284be454bcc0c36edbd6575b4fc
Glenn Gallien <glenn.gallien@pangea.cloud>
2022-07-23 14:43:31 -0700

    Add search result verification support.

--- examples/redact.ts
+++ examples/redact.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import PangeaConfig from "../src/config";
 import RedactService from "../src/services/redact";
 
@@ -10,10 +12,8 @@ const redact = new RedactService(token, config);
   const response = await redact.redact("Jenny Jenny... 415-867-5309");
 
   if (response.success) {
-    // eslint-disable-next-line no-console
     console.log("Success:", response.result);
   } else {
-    // eslint-disable-next-line no-console
     console.log("Error", response.code, response.result);
   }
 })();
